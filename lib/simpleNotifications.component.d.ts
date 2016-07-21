7dcb25de051194f8d580242c1315de4f70fd66e4
Christopher Creighton <ccreighton@apptio.com>
2016-07-21 10:58:00 -0700

    Scope CSS class names

---
+++ lib/simpleNotifications.component.d.ts
@@ -0,0 +1,40 @@
+import { OnInit, OnDestroy } from "@angular/core";
+import { Notification } from "./notification";
+import { NotificationsService } from "./notifications.service";
+import { Options } from "./options";
+export declare class SimpleNotificationsComponent implements OnInit, OnDestroy {
+    private _service;
+    constructor(_service: NotificationsService);
+    notifications: Notification[];
+    options: Options;
+    private listener;
+    private lastOnBottom;
+    private maxStack;
+    private preventLastDuplicates;
+    private preventDuplicates;
+    private timeOut;
+    private maxLength;
+    private clickToClose;
+    private showProgressBar;
+    private pauseOnHover;
+    private theClass;
+    private rtl;
+    private animate;
+    private expand;
+    private icons;
+    private lastNotificationCreated;
+    private onCreate;
+    private onDestroy;
+    ngOnInit(): void;
+    defaultBehavior(value: any): void;
+    add(item: any): void;
+    block(item: any): boolean;
+    attachChanges(): void;
+    buildEmit(notification: Notification, to: boolean): {
+        createdOn: Date;
+        type: string;
+        id: string;
+    };
+    cleanSingle(id: string): void;
+    ngOnDestroy(): void;
+}
